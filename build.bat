c02418f2ba4f09019722bbb53dfdf6e6e9727e9d
Nuggy <franco.ferriello@gmail.com>
2023-02-03 18:37:24 +0100

    updated building shortcuts

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 gradlew build
-cp "F:\JavaProjects\Raven-bPLUS\build\libs\[1.8.9] BetterKeystrokes V-1.2.jar" "C:\Users\Foodo\AppData\Roaming\.minecraft\mods\"
+cp "C:\Users\foodo\Documents\GitHub\Raven-bPLUS\build\libs\[1.8.9] BetterKeystrokes V-1.2.jar" "C:\Users\Foodo\AppData\Roaming\.minecraft\mods\"
 echo DONE
--- build.ps1
+++ build.ps1
@@ -1,1 +1,1 @@
-./gradlew build && cp 'F:\JavaProjects\Raven-bPLUS\build\libs\`[1.8.9`] BetterKeystrokes V-1.2.jar' $env:APPDATA\.minecraft\mods\ && echo Done
+./gradlew build && cp 'C:\Users\foodo\Documents\GitHub\Raven-bPLUS\build\libs\[1.8.9] BetterKeystrokes V-1.2.jar' $env:APPDATA\.minecraft\mods\ && echo Done
